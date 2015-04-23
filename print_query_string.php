e70798918b46b8da7d0a74979d6f35e7f95c1d9b
Veter2508 <Veter2508@gmail.com>
2015-04-23 13:33:59 +0400

    Delete print_query_string.php

--- print_query_string.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header ("Content-Type: text/plain");
-	echo "Query string = '".$_SERVER["QUERY_STRING"]."'";
-    /* Запрос
-   HTTP 1.1 GET /print_query_string.php?my_name=Sarah&my_surname=Rever&message=British+are+coming+by+sea.
-   
-   Ответ
-   Query string = 'my_name=Sarah&my_surname=Rever&message=British+are+coming+by+sea.'*/
