0fd9163202e27a62ad295f8ead8348c60db56c87
Veter2508 <Veter2508@gmail.com>
2015-04-23 13:33:53 +0400

    Delete print_name.php

--- print_name.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header ("Content-Type: text/plain");
-	echo "Hello, Dear ".$_GET["name"]."!";
-	/* Запрос
-   HTTP 1.1 GET /print_name.php?name=Sarah
-
-   Ответ
-   Content-Type: text/plain
-   Hello, Dear Sarah!*/
