3d479618aed57b3df2fcc3b0f88a89f61cf35e9d
Veter2508 <Veter2508@gmail.com>
2015-04-24 21:56:08 +0300

    Update print_query_string.php

--- lw1/print_query_string.php
+++ lw1/print_query_string.php
@@ -1,5 +1,5 @@
 <?php
-    header ("Content-Type: text/plain");
+    header("Content-Type: text/plain");
     echo "Query string = '".$_SERVER["QUERY_STRING"]."'";
     /* Запрос
    HTTP 1.1 GET /print_query_string.php?my_name=Sarah&my_surname=Rever&message=British+are+coming+by+sea.
