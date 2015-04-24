16e3218df7b1ec5a705a75af505dbda15a57efd7
Veter2508 <Veter2508@gmail.com>
2015-04-24 21:55:09 +0300

    Update print_name.php

--- lw1/print_name.php
+++ lw1/print_name.php
@@ -1,6 +1,6 @@
 <?php
-    header ("Content-Type: text/plain");
-    echo "Hello, Dear ".$_GET["name"]."!";
+    header("Content-Type: text/plain");
+    echo "Hello, Dear ". $_GET["name"] . "!";
     /* Запрос
    HTTP 1.1 GET /print_name.php?name=Sarah
 
